972762d119c9ab6d3508b0d3c475a8c62f5ce956
Dajana <dajana.sk@hotmail.com>
2018-05-10 19:22:19 +0200

    Update load_naredni.py

--- scripts and database/load_naredni.py
+++ scripts and database/load_naredni.py
@@ -15,25 +15,25 @@ conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
 
 x = date.today() - timedelta(1)
 def spusti_zip():
-	my_file = Path("C:/Users/Stefan/Desktop/PROEKT VBS/podatoci/" + x.strftime('%Y%m%d') + ".export.csv")
+	my_file = Path("../data/" + x.strftime('%Y%m%d') + ".export.csv")
 	if my_file.exists():
-		print("veke spusten")
+		print("downloaded")
 	else:
-		print("se spusta")
+		print("downloading...")
 		print(x.strftime('%Y%m%d'))
 		zipurl = 'http://data.gdeltproject.org/events/' + x.strftime('%Y%m%d') + '.export.CSV.zip'
 		print(zipurl)
 		with urlopen(zipurl) as zipresp:
 			with ZipFile(BytesIO(zipresp.read())) as zfile:
-				zfile.extractall('C:/Users/Stefan/Desktop/PROEKT VBS/podatoci')
+				zfile.extractall('../data')
 		cur = conn.cursor()
 		with open('C:/Users/Stefan/Desktop/PROEKT VBS/podatoci/' + x.strftime('%Y%m%d') + '.export.csv', 'r', encoding="utf8") as f:
 			next(f)  
 			cur.copy_from(f, 'vbs.tmp_main_table', null='None')
 		conn.commit()
-		print("spusten ekstraktiran, staven vo tmp")
-		cur.execute('select * from vbs.polni_main()')
-		print ('tureno')
+		print("downloaded, extracted and set in the table tmp_main_table")
+		cur.execute('select * from vbs.fill_main()')
+		print ('inserted')
 	return;
 #schedule.every(2).minutes.do(spusti_zip)
 #schedule.every().hour.do(spusti_zip)
