3dae076a832b7d5fb8763dc2185a8b496cc1936b
Dajana <dajana.sk@hotmail.com>
2018-05-10 19:20:31 +0200

    Update load_dosegasni.py

--- scripts and database/load_dosegasni.py
+++ scripts and database/load_dosegasni.py
@@ -16,26 +16,27 @@ conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
 x = datetime.strptime('20160618', '%Y%m%d')
 
 while 1:
-		my_file = Path("C:/Users/Stefan/Desktop/PROEKT VBS/podatoci/" + x.strftime('%Y%m%d') + ".export.csv")
+		# my_file contains the path where the data (.csv file) should be downloaded
+		my_file = Path("../data/" + x.strftime('%Y%m%d') + ".export.csv")
 		if my_file.exists():
-			print("veke spusten 1")
+			print("1 is downloaded")
 		else:
-			print("se spusta")
+			print("downloading...")
 			print(x.strftime('%Y%m%d'))
 			zipurl = 'http://data.gdeltproject.org/events/' + x.strftime('%Y%m%d') + '.export.CSV.zip'
 			print(zipurl)
 			with urlopen(zipurl) as zipresp:
 				with ZipFile(BytesIO(zipresp.read())) as zfile:
-					zfile.extractall('C:/Users/Stefan/Desktop/PROEKT VBS/podatoci')
+					zfile.extractall('../data')
 			cur = conn.cursor()
 			cur.execute("truncate vbs.tmp_main_table")
-			with open('C:/Users/Stefan/Desktop/PROEKT VBS/podatoci/' + x.strftime('%Y%m%d') + '.export.csv', 'r', encoding="utf8") as f:
+			with open('../data' + x.strftime('%Y%m%d') + '.export.csv', 'r', encoding="utf8") as f:
 				next(f)  
 				cur.copy_from(f, 'vbs.tmp_main_table', null='None')
 			conn.commit()
-			print("spusten ekstraktiran, staven vo tmp")
-			cur.execute('select * from vbs.polni_main()')
-			print ('tureno')
+			print("downloaded, extracted and set in the table")
+			cur.execute('select * from vbs.fill_main()')
+			print ('inserted')
 			x += timedelta(days=1)
 print (x.strftime('%Y%m%d'))
 
